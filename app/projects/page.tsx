72078884e8447e39fd609d573e7ef69b5d439c86
Boripat Kunla <167314769+deltcrosx1024@users.noreply.github.com>
2025-09-17 13:16:10 +0700

    fix ui lines

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -96,11 +96,10 @@ export default async function Projects() {
                             ))}
                         </div>
                     </div>
-                    
                 </div>
             </section>
             <div className="pt-2 lg:pt-0">
-                <div className="px-6 pt-20 mx-auto space-y-8 max-w-7xl lg:px-8 md:space-y-16 md:pt-24 lg:pt-32">
+                <div className="px-6 mx-auto max-w-7xl lg:px-8">
                     <div className="w-full h-px bg-white"/>
                 </div>
                 
