999d5728126b55616cb0b7b1c8903b1671367fdf
Boripat Kunla <167314769+deltcrosx1024@users.noreply.github.com>
2025-09-23 21:29:02 +0700

    Fuck it for the background, I can use the fucken gradient

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,6 @@
 import Link from 'next/link';
 import { Header } from './components/nav';
 import SplitText from './components/splittext';
-import LiquidEther from './components/etherbg';
 
 const handleAnimationComplete = () => {
   console.log('Animation complete!');
@@ -14,12 +13,19 @@ export default function Home() {
     
     <div className="bg-gray-900 min-h-screen w-screen relative isolate overflow-hidden">
       <Header />
+      {/*Background Clipping*/}
       <div
-        aria-hidden="false"
-        className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden sm:-top-80 w-screen h-screen"
+          aria-hidden="true"
+          className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80 w-screen h-[100%]"
       >
-        <LiquidEther className="absolute inset-0 w-screen h-screen" />
-      </div>
+          <div
+              style={{
+                  clipPath:
+                      'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
+              }}
+              className="relative left-[calc(50%-11rem)] aspect-1155/678 w-144.5 -translate-x-1/2 rotate-30 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-288.75"
+          />
+            </div>
       <div className="flex flex-col items-center justify-center min-h-screen px-6 py-14 lg:px-8 text-center animate-fade-in">
         <div className="relative isolate">
           <div className="hidden sm:mb-8 sm:flex sm:justify-center animate-fade-in-down">
