2cb222cbd569bbfccb4d42a8b6fd3f6399a3661d
Boripat Kunla <167314769+deltcrosx1024@users.noreply.github.com>
2025-09-28 07:52:08 +0700

    incr update

--- pages/api/incr.ts
+++ pages/api/incr.ts
@@ -41,7 +41,7 @@ export default async function incr(req: NextRequest): Promise<NextResponse> {
     // deduplicate the ip for each slug
     const isNew = await redis.set(["deduplicate", hash, slug].join(":"), true, {
       nx: true,
-      ex: 24 * 60 * 60,
+      px: 200,
     });
     if (!isNew) {
       return new NextResponse(null, { status: 202 });
